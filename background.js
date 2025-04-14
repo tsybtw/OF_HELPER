bf75aa781ecc61927fa8605072c330e0e6b6aca2
ppleaser <nikitadolzhenko03@gmail.com>
2025-04-14 07:22:04 +0300

    v. 5.6.4 - FFMPEG changes

--- background.js
+++ background.js
@@ -3341,7 +3341,7 @@ async function setBind(tab, DELAY_GREEN_BUTTON) {
           });
 
             function updateVersionText(activeBrowser) {
-            const VERSION = '5.6.3.3';
+            const VERSION = '5.6.4';
             versionContainer.textContent = `version: ${VERSION} | browser: ${activeBrowser}`;
             }
         
