918a9c3eef235baeeeceb9931c51edb555418728
ppleaser <nikitadolzhenko03@gmail.com>
2025-06-16 01:39:00 +0300

    v. 5.7.0.2 - Copy tags

--- background.js
+++ background.js
@@ -3454,7 +3454,7 @@ async function setBind(tab, DELAY_GREEN_BUTTON) {
           });
 
             function updateVersionText(activeBrowser) {
-            const VERSION = '5.7.0.1';
+            const VERSION = '5.7.0.2';
             versionContainer.textContent = `version: ${VERSION} | browser: ${activeBrowser}`;
             }
 
