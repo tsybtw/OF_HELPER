8540dc560863279df7377a079c977565eb1779af
ppleaser <nikitadolzhenko03@gmail.com>
2025-07-13 20:17:16 +0300

    v. 5.7.2.2 - Delete * around nickname

--- background.js
+++ background.js
@@ -3475,7 +3475,7 @@ async function setBind(tab, DELAY_GREEN_BUTTON) {
           });
 
             function updateVersionText(activeBrowser) {
-            const VERSION = '5.7.2.1';
+            const VERSION = '5.7.2.2';
             versionContainer.textContent = `version: ${VERSION} | browser: ${activeBrowser}`;
             }
 
