5bbd3e97753fe2dcde5ca3077ac740e712906e76
ppleaser <nikitadolzhenko03@gmail.com@>
2025-10-29 23:12:25 +0200

    v. 5.8.6.4 - Auto unzip, skip files

--- background.js
+++ background.js
@@ -3505,7 +3505,7 @@ async function setBind(tab, DELAY_GREEN_BUTTON) {
           });
 
             function updateVersionText(activeBrowser) {
-            const VERSION = '5.8.6.3';
+            const VERSION = '5.8.6.4';
             versionContainer.textContent = `version: ${VERSION} | browser: ${activeBrowser}`;
             }
 
