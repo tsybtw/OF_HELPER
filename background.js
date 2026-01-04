2d335b07baf03b6fcb474229eb37b32dbceb355f
tsybtw <nikitadolzhenko03@gmail.com>
2026-01-04 16:50:24 +0200

    v. 5.8.8.7 - One message

--- background.js
+++ background.js
@@ -4175,7 +4175,7 @@ async function setBind(tab, DELAY_GREEN_BUTTON) {
           });
 
             function updateVersionText(activeBrowser) {
-            const VERSION = '5.8.8.6';
+            const VERSION = '5.8.8.7';
             versionContainer.textContent = `version: ${VERSION} | browser: ${activeBrowser}`;
             }
 
